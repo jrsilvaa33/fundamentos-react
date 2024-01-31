52b6e4ecd390be51d40176d73c0913cbf1524251
jrsilvaa33 <juuninho_81@hotmail.com>
2024-01-31 01:40:31 -0300

    Estrutura da aplicação - Componente: Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
-import { Post } from "./Post";
-import { Header } from "./components/Header";
+import { Post } from './Post';
+import { Header } from './components/Header';
+
+
+import styles from "./App.module.css";
+
 import "./global.css"
+import { Sidebar } from './components/Sidebar';
 
 function App() {
   return (
@@ -8,15 +13,30 @@ function App() {
 
       <Header />
       
+
+      <div className={styles.wrapper}>
+      <Sidebar />
+      
+      
+      <main>
+
+
       <Post
         author="Diego Fernandes"
         contest="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, ratione libero rerum, voluptatibus beatae pariatur quasi, illum debitis culpa cupiditate non labore et quaerat deserunt aperiam dolore nisi temporibus delectus?"
       />
 
+
+
       <Post
       author="José Rildo"
       contest="Conteúdo novo e de interesse geral"
       />
+
+
+      </main>
+
+      </div>
     </div>
   )
 }
